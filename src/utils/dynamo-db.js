7212f05e43d23160ffc1a2b67ea2d1cf570dc208
epiqueras <epiquerass@gmail.com>
2018-07-25 10:29:35 -0500

    feat(user-settings): implement patch

--- src/utils/dynamo-db.js
+++ src/utils/dynamo-db.js
@@ -5,5 +5,6 @@ const { DynamoDB } = require('aws-sdk')
 const dynamoDB = new DynamoDB({ apiVersion: '2012-08-10', region: 'us-east-2' })
 dynamoDB.getItem = promisify(dynamoDB.getItem)
 dynamoDB.putItem = promisify(dynamoDB.putItem)
+dynamoDB.updateItem = promisify(dynamoDB.updateItem)
 
 export default dynamoDB
