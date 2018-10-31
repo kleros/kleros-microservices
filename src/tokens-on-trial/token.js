2e2091f70fa9f2733282763b769ff373ee3710ed
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-10-31 10:32:46 -0300

    feat(t2cr): store token data and return

--- src/tokens-on-trial/token.js
+++ src/tokens-on-trial/token.js
@@ -1,9 +1,36 @@
-module.exports.post = async (event, _context, callback) => {
+const _web3 = require('../utils/web3')
+
+module.exports.patch = async (event, _context, callback) => {
+  const web3 = await _web3()
+
+  const payload = JSON.parse(event.body).payload
+  const tokenID = web3.sha3(JSON.stringify(payload.token), { encoding: 'hex' })
+
+  const keys = [
+    'name',
+    'ticker',
+    'address',
+    'uri',
+  ].filter(
+    k => payload.token[k]
+  )
   callback(null, {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({
-        message: 'All systems go'
-      })
+    body: JSON.stringify({
+      payload: {
+        tokenID,
+        token: await dynamoDB.putItem({
+          Key: { tokenID: { S: tokenID } },
+          TableName: 'token',
+          UpdateExpression: keys.map(k => `SET ${k} = :_${k}`).join(', '),
+          ExpressionAttributeValues: keys.reduce((acc, k) => {
+            acc[`:_${k}`] = payload.token[k]
+            return acc
+          }, {}),
+          ReturnValues: 'ALL_NEW'
+        })
+      }
+    })
   })
 }
