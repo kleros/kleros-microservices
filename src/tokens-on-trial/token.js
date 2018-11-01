8e16a33b2fd3702c83b1a3ff628a6ec76ed74edd
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-11-01 15:39:25 -0300

    fix(t2cr): swap patch for put
    A token's data will never be updated since their ID is the hash of the data.

--- src/tokens-on-trial/token.js
+++ src/tokens-on-trial/token.js
@@ -1,36 +1,36 @@
 const _web3 = require('../utils/web3')
+const dynamoDB = require('../utils/dynamo-db')
 
-module.exports.patch = async (event, _context, callback) => {
+module.exports.put = async (event, _context, callback) => {
   const web3 = await _web3()
 
   const payload = JSON.parse(event.body).payload
   const tokenID = web3.sha3(JSON.stringify(payload.token))
+  const { name, ticker, address, URI } = payload.token
+  const token = {
+    tokenID: { S: tokenID },
+    name: { S: name },
+    ticker: { S: ticker },
+    address: { S: address },
+    URI: { S: URI }
+  }
 
-  const keys = [
-    'name',
-    'ticker',
-    'address',
-    'URI',
-  ].filter(
-    k => payload.token[k]
-  )
-  callback(null, {
-    statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify({
-      payload: {
-        tokenID,
-        token: await dynamoDB.putItem({
-          Key: { tokenID: { S: tokenID } },
-          TableName: 'token',
-          UpdateExpression: keys.map(k => `SET ${k} = :_${k}`).join(', '),
-          ExpressionAttributeValues: keys.reduce((acc, k) => {
-            acc[`:_${k}`] = payload.token[k]
-            return acc
-          }, {}),
-          ReturnValues: 'ALL_NEW'
-        })
-      }
+  try {
+    await dynamoDB.putItem({
+      TableName: 'token',
+      Item: token
     })
-  })
+
+    callback(null, {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ payload: { token } })
+    })
+  } catch (err) {
+    return callback(null, {
+      statusCode: 403,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err })
+    })
+  }
 }
