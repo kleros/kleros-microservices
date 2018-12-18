cdc22d7c58ac6c13a0256d53fb54412a54a5d676
epiqueras <epiquerass@gmail.com>
2018-12-18 19:13:17 +0000

    feat(cfs): validate multihashes and enforce file limit and mime type

--- src/global/cfs.js
+++ src/global/cfs.js
@@ -1,10 +1,44 @@
 const { promisify } = require('util')
 
+const Archon = require('@kleros/archon').default
 const AWS = require('aws-sdk')
 
 module.exports.post = async (event, _context, callback) => {
-  // Parse input
+  // Parse input and validate hash
   const { fileName, base64EncodedData } = JSON.parse(event.body).payload
+  if (!Archon.utils.validMultihash(fileName, base64EncodedData))
+    return callback(null, {
+      statusCode: 403,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        error: 'Invalid file hash.'
+      })
+    })
+
+  // Parse image
+  let _match, mimeType, base64Data
+  try {
+    ;[_match, mimeType, base64Data] = base64EncodedData.match(
+      /^data:(.+);base64,(.+)$/
+    )
+  } catch (err) {
+    console.error(err)
+    return callback(null, {
+      statusCode: 400,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: 'Invalid base64 encoded data.' })
+    })
+  }
+
+  // Check size
+  if (base64Data.length * (3 / 4) > 1e7)
+    return callback(null, {
+      statusCode: 400,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        error: 'File is too big. It has to be smaller than 1000KB.'
+      })
+    })
 
   // Upload file to S3 bucket
   AWS.config.update({ region: process.env.AWS_REGION })
@@ -14,8 +48,9 @@ module.exports.post = async (event, _context, callback) => {
   bucket.upload = promisify(bucket.upload)
   const { Location: location } = await bucket.upload({
     Key: fileName,
-    Body: Buffer.from(base64EncodedData, 'base64'),
+    Body: Buffer.from(base64Data, 'base64'),
     ContentEncoding: 'base64',
+    ContentType: mimeType,
     ACL: 'public-read'
   })
 
