7d4fdf270e91859bf03e9edfcaa5b01337d61beb
Nicolas Wagner <contact@wagner-nicolas.com>
2019-01-16 15:14:24 +0100

    feat(escrow-notification): add escrow dispute notification

--- src/global/user-settings.js
+++ src/global/user-settings.js
@@ -37,7 +37,8 @@ module.exports.patch = async (event, _context, callback) => {
     'courtNotificationSettingLose',
     'courtNotificationSettingWin',
     'centralizedArbitratorDashboardNotificationSettingDisputes',
-    'centralizedArbitratorDashboardNotificationSettingEvidence'
+    'centralizedArbitratorDashboardNotificationSettingEvidence',
+    'escrowNotificationSettingRaiseDispute'
   ].filter(k => payload.settings[k])
   callback(null, {
     statusCode: 200,
